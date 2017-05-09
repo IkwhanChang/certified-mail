f56ad8ca8fea8854a7f81baadf7e6d5dd52b4050
changmatthew <matthew.chang@me.com>
2017-05-09 15:08:37 -0700

    test

--- index.js
+++ index.js
@@ -10,11 +10,13 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('pages/index');
+  response.render('pages/inbox');
+});
+
+app.get('/compose', function(request, response) {
+  response.render('pages/compose');
 });
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
-
-
